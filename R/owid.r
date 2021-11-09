9183fcc1e767841d5027277637e169e385288f60
martinpastoors <martin.pastoors@gmail.com>
2021-11-09 05:55:30 +0100

    updates

--- R/owid.r
+++ R/owid.r
@@ -15,34 +15,20 @@ owid <-
 # OxCGRT <- 
 #   read.csv("https://github.com/OxCGRT/covid-policy-tracker/blob/master/data/OxCGRT_withnotes_2021.csv")
 
-# facet grid with cases, people vaccinated and stringency index
-owid %>%
-  filter(iso_code %in% c("DEU","NLD", "BEL", "FRA", "ESP","DNK")) %>% 
-  filter(date >= lubridate::ymd("2021-01-01")) %>% 
-  dplyr::select(iso_code, date, 
-                new_cases_smoothed_per_million, stringency_index, people_fully_vaccinated_per_hundred) %>% 
-  pivot_longer(names_to = "variable", values_to = "value", 
-               new_cases_smoothed_per_million:people_fully_vaccinated_per_hundred) %>% 
-  filter(value >= 0) %>% 
 
-  ggplot(aes(x = date)) +
-  theme_bw() +
-  theme(legend.position = "none") +
-  geom_line(aes(y =value, colour=variable)) +
-  # geom_point(aes(y=value, colour=variable), size=0.3) +
-  expand_limits(y=0) +
-  facet_grid(variable~iso_code, scales="free_y")
 
 # patchwork plot with cases, people vaccinated and stringency index
 t <-
   owid %>%
-  filter(iso_code %in% c("DEU","NLD", "BEL", "FRA", "ESP","DNK")) %>% 
+  filter(iso_code %in% c("DEU","NLD", "BEL", "FRA", "ESP","DNK", "IRL", "GBR")) %>% 
   filter(date >= lubridate::ymd("2021-01-01")) %>% 
   dplyr::select(iso_code, date, 
-                new_cases_smoothed_per_million, stringency_index, people_fully_vaccinated_per_hundred) %>% 
+                new_cases_smoothed_per_million, new_cases_per_million, stringency_index, people_fully_vaccinated_per_hundred) %>% 
   pivot_longer(names_to = "variable", values_to = "value", 
                stringency_index:people_fully_vaccinated_per_hundred) %>% 
-  filter(value >= 0, new_cases_smoothed_per_million > 0) 
+  filter(value >= 0, (new_cases_per_million > 0 | new_cases_smoothed_per_million > 0)) 
+
+t %>% group_by(iso_code) %>% filter(iso_code == "NLD", date == max(date)) %>% dplyr::select(iso_code, date) %>% View()
 
 p1 <-
   t %>% 
@@ -50,11 +36,20 @@ p1 <-
   theme_bw() +
   theme(legend.position = "none") +
   theme(axis.text.x = element_blank()) +
-  geom_line(aes(y =new_cases_smoothed_per_million), colour="black") +
-  ggrepel::geom_label_repel(data=t %>% group_by(iso_code) %>% filter(date==max(date, na.rm=TRUE)), 
-                            aes(y=new_cases_smoothed_per_million, label=round(new_cases_smoothed_per_million))) +
+  
+  geom_point(aes(y =new_cases_per_million), colour="black", size=0.2) +
+
+  geom_line(aes(y =new_cases_smoothed_per_million), colour="red") +
+  ggrepel::geom_label_repel(data=t %>% group_by(iso_code) %>% filter(date==max(date, na.rm=TRUE)),
+                            aes(y=new_cases_smoothed_per_million, label=round(new_cases_smoothed_per_million)),
+                            nudge_y = 500,
+                            segment.color = "red",
+                            segment.size = 0.2,
+                            segment.linetype = "dashed",
+                            colour="red") +
+  labs(y="", x="", title = "new_cases_smoothed_per_million") +
+  
   expand_limits(y=0) +
-  labs(y="", x="", title = "new_cases_smoothed_per_million") + 
   facet_wrap(~iso_code, nrow=1)
 
 p2 <-
@@ -73,3 +68,21 @@ p2 <-
 p1 / p2
 
 
+
+# facet grid with cases, people vaccinated and stringency index
+# owid %>%
+#   filter(iso_code %in% c("DEU","NLD", "BEL", "FRA", "ESP","DNK")) %>% 
+#   filter(date >= lubridate::ymd("2021-01-01")) %>% 
+#   dplyr::select(iso_code, date, 
+#                 new_cases_smoothed_per_million, stringency_index, people_fully_vaccinated_per_hundred) %>% 
+#   pivot_longer(names_to = "variable", values_to = "value", 
+#                new_cases_smoothed_per_million:people_fully_vaccinated_per_hundred) %>% 
+#   filter(value >= 0) %>% 
+#   
+#   ggplot(aes(x = date)) +
+#   theme_bw() +
+#   theme(legend.position = "none") +
+#   geom_line(aes(y =value, colour=variable)) +
+#   # geom_point(aes(y=value, colour=variable), size=0.3) +
+#   expand_limits(y=0) +
+#   facet_grid(variable~iso_code, scales="free_y")
--- R/tidycovid.r
+++ R/tidycovid.r
@@ -5,17 +5,31 @@ library(tidycovid19)
 library(zoo)
 library(patchwork)
 
-df <- 
-  tidycovid19::download_merged_data(cached = TRUE, silent = TRUE) %>% 
-  mutate(
-    new_cases_per_million = 100000*(confirmed - lag(confirmed))/population,
-    new_cases_smoothed_per_million = zoo::rollmean(new_cases_per_million, 7, na.pad=TRUE, align="right")
-  )
+df <- tidycovid19::download_merged_data(cached = TRUE, silent = TRUE) 
   
-df %>%
-  filter(iso3c == "NLD") %>%
+t <-
+  df %>% 
+  group_by(iso3c) %>% 
+  mutate(new_cases_per_million = 1000000*(confirmed - lag(confirmed))/population) %>% 
+  mutate(new_cases_per_million = ifelse(new_cases_per_million<0, 0, new_cases_per_million)) %>% 
+  mutate(new_cases_smoothed_per_million = zoo::rollmean(new_cases_per_million, 7, na.pad=TRUE, align="right")) %>% 
+  filter(iso3c %in% c("NLD","IRL","DEU","FRA","GBR","AUT","DNK")) %>%
+  filter(date >= lubridate::ymd("2021-01-01")) 
+  
+t %>%
   ggplot(aes(x = date)) +
+  theme_bw() +
   geom_bar(aes(y = new_cases_per_million), stat = "identity", fill = "lightblue") +
   geom_line(aes(y = new_cases_smoothed_per_million), color ="red") +
-  theme_minimal()
+  ggrepel::geom_label_repel(data=t %>% group_by(iso3c) %>% filter(date==max(date, na.rm=TRUE)),
+                            aes(y=new_cases_smoothed_per_million, label=round(new_cases_smoothed_per_million)),
+                            nudge_y = 500,
+                            segment.color = "red",
+                            segment.size = 0.2,
+                            segment.linetype = "dashed",
+                            colour="red") +
+  facet_wrap(~iso3c, nrow=1)
 
+
+
+df %>% group_by(iso3c) %>% filter(iso3c=="NLD", date == max(date, na.rm=TRUE)) %>% dplyr::select(iso3c, date)
